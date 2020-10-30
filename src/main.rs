581caba0a66cc90d8fc1ae1757c67391d10b8447
Jeffrey Perkel <j_perkel@yahoo.com>
2020-10-30 14:26:26 -0600

    Move graphing to separate function

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,13 @@ use bio::io::fasta;
 // index = (16 * first_base) + (4 * second_base) + third_base 
 // So... AAA = 0, AAC = 1, AAG = 2, ... , TTC = 61, TTG = 62, TTT = 63
 const GENETIC_CODE: &str = "KNKNTTTTRSRSIIMIQHQHPPPPRRRRLLLLEDEDAAAAGGGGVVVV*Y*YSSSS*CYCLFLF"; 
-const ERR_NT: usize = 99; 
 
+// ERR_BAD_NT is an error value for an invalid nucleotide 
+const ERR_BAD_NT: usize = 99; 
+
+
+// calculate the first x Fibonacci numbers
 fn fibonacci(x: usize) -> Vec<usize> {
-    // calculate the first x Fibonacci numbers
     assert!(x >= 2, "x must be at least 2!");
     let mut f1 = 0;
     let mut f2 = 1;
@@ -30,76 +33,12 @@ fn fibonacci(x: usize) -> Vec<usize> {
     return ar;
 }
 
-fn lookup(x: char) -> usize {
-    let base;
-
-    match x {
-        'A' => base = 0,
-        'C' => base = 1,
-        'G' => base = 2,
-        'T' => base = 3,
-        _ => base = ERR_NT,
-    }
-    return base;
-}
-
-fn translate(triplet: &str) -> char {
-    let mut codon = vec![99; 3];
-
-    for (i,base) in triplet.chars().enumerate() {
-        let val = lookup(base);
-        if val == ERR_NT {
-            panic!(); // invalid character
-        }
-        codon[i] = val;
-    }
-
-    let index: usize = (codon[0] * 16) + (codon[1] * 4) + codon[2];
-    return GENETIC_CODE.chars().nth(index).unwrap();
-}
-
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Reading file sequence.fasta...");
-    let reader = fasta::Reader::from_file("sequence.fasta")?;
-
-    let mut nb_reads = 0;
-    let mut nb_bases = 0;
-    let mut s = String::new();
-
-    for record in reader.records() {
-        let record = record.expect("Error during FASTA record parsing");
-        if record.id() == "NC_005816.1" {
-            println!("{:?}", record.desc());
-            let myseq = &record.seq()[3485..3857];
-            s = String::from_utf8(myseq.to_vec())?;
-        }
-        nb_reads += 1;
-        nb_bases += record.seq().len();
-    }
-    println!("Total number of reads: {}", nb_reads);
-    println!("Total number of bases: {}\n", nb_bases);
-
-    println!("Sequence of my gene [3485..3857]:");
-    println!("{}", s);
-    println!("Length: {}\n", s.len());
-
-    println!("Translation of my gene:");
-    let mut peptide = String::new();    
-    for i in 0..s.len()/3 {
-        let codon = &s[i*3..(i*3)+3];
-        peptide.push(translate(&codon));
-    }
-    println!("{}", peptide);
-    println!("Length: {}\n", peptide.len());
-
-    let n = 25; // number of Fibonacci numbers to compute
-    let f = fibonacci(n); 
-
-    println!("The first {} Fibonacci numbers:\n{:?}", n, f);
-
+// graph the Fibonacci sequence
+fn fibonacci_plot(f: Vec<usize>) -> Result<(), Box<dyn std::error::Error>> {
+    let n = f.len();
     // a vector of points to plot
     let mut v = vec![(0.0,0.0); n];
+
     for i in 0..n {
         v[i] = (i as f32, f[i] as f32);
     }
@@ -130,6 +69,82 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .draw_series(LineSeries::new(v.iter()
             .map(|(x,y)| (*x,*y)), &RED,
     ))?;
+
+    Ok(()) 
+}
+
+// given an input 'char', return a base equivalent
+fn lookup(x: char) -> usize {
+    let base;
+
+    match x {
+        'A' => base = 0,
+        'C' => base = 1,
+        'G' => base = 2,
+        'T' => base = 3,
+        _ => base = ERR_BAD_NT, // unknown base
+    }
+    return base;
+}
+
+// translate a codon into a single-letter amino acid
+fn translate(triplet: &str) -> char {
+    let mut codon = vec![99; 3];
+
+    for (i,base) in triplet.chars().enumerate() {
+        let val = lookup(base);
+        if val == ERR_BAD_NT {
+            panic!(); // invalid character
+        }
+        codon[i] = val;
+    }
+
+    let index: usize = (codon[0] * 16) + (codon[1] * 4) + codon[2];
+    return GENETIC_CODE.chars().nth(index).unwrap();
+}
+
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Calculating Fibonacci sequence...\n");
+    let n = 25; // number of Fibonacci numbers to compute
+    let f = fibonacci(n); 
+    println!("The first {} Fibonacci numbers:\n{:?}\n", n, f);
+    fibonacci_plot(f)?;
+
+    println!("Reading sequence data...");
+    let reader = fasta::Reader::from_file("sequence.fasta")?;
+
+    let mut nb_reads = 0;
+    let mut nb_bases = 0;
+    let mut s = String::new();
+
+    for record in reader.records() {
+        let record = record.expect("Error during FASTA record parsing");
+        if record.id() == "NC_005816.1" {
+            println!("{:?}", record.desc());
+            let myseq = &record.seq()[3485..3857];
+            s = String::from_utf8(myseq.to_vec())?;
+        }
+        nb_reads += 1;
+        nb_bases += record.seq().len();
+    }
+    println!("Total number of reads: {}", nb_reads);
+    println!("Total number of bases: {}\n", nb_bases);
+
+    println!("Sequence of my gene [3485..3857]:");
+    println!("{}", s);
+    println!("Length: {}\n", s.len());
+
+    println!("Translation of my gene:");
+    assert!(s.len() % 3 == 0, "Sequence is not a multiple of 3 in length!");
+    let mut peptide = String::new();    
+    for i in 0..s.len()/3 {
+        let codon = &s[i*3..(i*3)+3];
+        peptide.push(translate(&codon));
+    }
+    println!("{}", peptide);
+    println!("Length: {}\n", peptide.len());
 
     Ok(()) 
 }
